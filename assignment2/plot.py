bbeed6ed5efa818e137fbba8c623c2a683c95904
arneeev <134218116+arneeev@users.noreply.github.com>
2023-05-23 11:56:12 +0100

    assignment 2 all files

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,16 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="pingprocessed.data"
-label='ping'
-ylabel = 'ping test number'
-xlabel = 'time (ms)'
-title='Ping, 0.01s interval'
-fig_name='ping0.01.png'
+#filename="iperf3_tcp_proc.data"
+label='iperf3'
+ylabel = 'Percentage of packets lost'
+xlabel = 'Bandwidth (Mbits/sec)'
+title='iperf3 UDP percentage of packets dropped at different bandwidths'
+fig_name='iperf3_packets.png'
+k=[0.1,1,100]
+t=[0,0,0]
 
-
-t = np.loadtxt(filename, delimiter=" ", dtype="float")
-k = np.array(range(1, 1001))
+#t = np.loadtxt(filename, delimiter=" ", dtype="float")
+#k = np.array(range(1, 11))
 plt.plot(k, t, label=label)  # Plot some data on the (implicit) axes.
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
